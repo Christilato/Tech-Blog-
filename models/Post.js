2d9e054b3f0c6e4f9e760be667b3fb2815c727d0
Christi Lato <christilato@Christis-MacBook-Air.local>
2022-10-09 14:03:18 -0500

    added code into models

--- models/Post.js
+++ models/Post.js
@@ -3,6 +3,76 @@ const sequelize = require('../config/connection');
 
 class Post extends Model {}
 
-// define the table columns and configuration, similar to the setup for the User model
-
+Post.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,        
+      },
+      post_text: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            len: [1]
+        },
+      },
+      user_id: {
+        type: DataTypes.STRING,
+        references: {
+            model: 'user',
+            key: 'id'
+        }        
+      },
+    },
+    {
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'post',
+    }
+  );
+  Post.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,        
+      },
+      post_text: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            len: [1]
+        },
+      },
+      user_id: {
+        type: DataTypes.STRING,
+        references: {
+            model: 'user',
+            key: 'id'
+        }        
+      },
+    },
+    {
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'post',
+    }
+  );
 module.exports = Post;
