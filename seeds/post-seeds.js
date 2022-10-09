a0c70a493cce830d046fcb2bdaba0f3e11f8a71f
Christi Lato <christilato@Christis-MacBook-Air.local>
2022-10-09 13:48:15 -0500

    added data into my seeds

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -1,12 +1,36 @@
 const { Post } = require('../models');
 
 const postData = [ 
-    // {
-    //     title:
-    //     post_content:
-    //     user_id:
-    // }
-    // do this 6 times
+    {
+        title: "Express",
+        post_content:"The Express philosophy is to provide small, robust tooling for HTTP servers, making it a great solution for single page applications, websites, hybrids, or public HTTP APIs.",
+        user_id:"1"
+    }, 
+    {
+        title: "Mysql2",
+        post_content: "MySQL2 is free from native bindings and can be installed on Linux, Mac OS or Windows without any issues.",
+        user_id: "2"
+    },
+    {
+        title: "Sequelize",
+        post_content: "Sequelize is an easy-to-use and promise-based Node.js ORM tool. It features solid transaction support, relations, eager and lazy loading, read replication and more.", 
+        user_id: "3"
+    }, 
+    {
+        title: "B-crypt",
+        post_content: "A library to help you hash passwords.",
+        user_id: "4"
+    }, 
+    {
+        title: "dotenv",
+        post_content: "Dotenv is a zero-dependency module that loads environment variables from a .env file into process.env. Storing configuration in the environment separate from code is based on The Twelve-Factor App methodology.",
+        user_id: "5"
+    }, 
+    {
+     title: "Handlebars", 
+     post_content: "Handlebars.js is a logicless templating languages that keep the view and the code separated like we all know they should be.", 
+     user_id: "6"   
+    }    
 
 ]
 
--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -3,13 +3,40 @@ const { User } = require('../models');
 const userData = [
     {
         username: "christi_lato", 
-        twitter: "christil",
+        twitter: "clato",
         github: "Christilato",
         password: "p@ssword1"
+    },
+    {
+        username: "TerrenceB", 
+        twitter: "Tb",
+        github: "TerrenceBudnik",
+        password: "p@ssword2"
+    },
+    {
+        username: "anak", 
+        twitter: "ak2",
+        github: "akowala",
+        password: "p@ssword3"
+    },
+    {
+        username: "yasmin", 
+        twitter: "y2k",
+        github: "yazbustami",
+        password: "p@ssword4"
+    },
+    {
+        username: "Tyler-Esselman", 
+        twitter: "tylerE",
+        github: "tylseresselman-sl",
+        password: "p@ssword5"
+    },
+    {
+        username: "Paul-Cwik", 
+        twitter: "yourboypaul",
+        github: "PCeeezy",
+        password: "p@ssword6"
     }
-
-
-    // make 6
 ]
 
 const seedUsers = () => User.bulkCreate(userData);
