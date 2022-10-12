81c27d78bea4cac7d8db1e1e9e5440c73a188d6a
Christi Lato <christilato@Christis-MacBook-Air.local>
2022-10-12 14:53:11 -0500

    capital S would you believe it

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 const sequelize = process.env.JAWSDB_URL
-  ? new sequelize(process.env.JAWSDB_URL)
+  ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
